70962cf64f9d82edb8c8261e1a8653bdae717cd8
parmar.ja@northeastern.edu <jatinparmar06@gmail.com>
2023-03-02 17:26:00 -0500

    Query updated

--- src/main/java/com/example/assignment1/repository/ImageRepository.java
+++ src/main/java/com/example/assignment1/repository/ImageRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
-    @Query(value="SELECT * FROM IMAGE WHERE product_id =?1",
+    @Query(value="select * from image where product_id =?1",
             nativeQuery = true)
     List<Image> findImageByProductId(Integer productId);
 }
