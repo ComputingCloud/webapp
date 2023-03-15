1e9cdc4d0d391f4833b52e17e3a0bf67b5afa0bf
parmar.ja@northeastern.edu <jatinparmar06@gmail.com>
2023-03-15 09:34:24 -0400

    removed extras lines

--- src/main/java/com/example/assignment1/config/AWSConfig.java
+++ src/main/java/com/example/assignment1/config/AWSConfig.java
@@ -1,23 +1,13 @@
 package com.example.assignment1.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import jakarta.persistence.Entity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSConfig {
-
-//    @Value("${aws.accessKey}")
-//    private String accessKey;
-//
-//    @Value("${aws.secretKey}")
-//    private String accessSecret;
     @Value("${aws.region}")
     private String region;
 
